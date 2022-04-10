69fbbef8d3bbb482309c437137131ab00d2f690c
qasem <2171246jo@s.aou.edu.jo>
2022-04-10 12:18:28 +0300

    node

--- nodejs/bodayParser.js
+++ nodejs/bodayParser.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 const app = express();
-
-app.get('/', (req, res,next) => {
+bodyParser = bodyParser.urlencoded({extended: true});
+app.get('/', (req, res, next) => {
     res.send('<form action="/form" method="post"><input  type="text" name="user" /><input type="submit" value="Submit" /></form>');
 });
 
 
-app.post('/form', (req, res,next) => {
-    // res.send('<h1>Hello ' + req.body.user + '</h1>');
+app.post('/form', bodyParser, (req, res, next) => {
+    console.log(req.body.user);
+    res.end()
 });
 
 app.listen(5000, () => {
