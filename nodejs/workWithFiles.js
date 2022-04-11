4d9d0a86cf5e908d2fa42d2d0eb15784ca00336c
qasem <2171246jo@s.aou.edu.jo>
2022-04-12 02:32:26 +0300

    node

--- nodejs/workWithFiles.js
+++ nodejs/workWithFiles.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const app=express()
-
+const path=require('path')
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/views/index.html')
+    res.sendFile(path.join(__dirname,'views','index.html'))
 })
+
 app.get('about',(req,res)=>{
     res.send('About')
 })
